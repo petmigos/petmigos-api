0ec56038fff0edc78fe6b968cd31ca7f1bcf93ae
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2023-01-30 12:42:29 -0400

    add project example

--- src/api/routes/hello.ts
+++ src/api/routes/hello.ts
@@ -1,7 +1,16 @@
-import { Router } from "express";
+import { Request, Router } from "express";
+import { SayHello } from "../../domain/useCases/say_hello";
+import { HelloService } from "../services/hello_service";
 
 export const HelloRouter = Router();
 
-HelloRouter.get("/", (request, response) => {
-  return response.json({ hello: "world" });
-});
+HelloRouter.get(
+  "/",
+  (request: Request<{}, {}, {}, { name?: string }>, response) => {
+    const { name } = request.query;
+    const helloService = new HelloService();
+    const sayHello = new SayHello(helloService);
+
+    return response.json({ hello: sayHello.execute(name) });
+  }
+);
