175304b8848f804c3aa5319a7bf4ddb04443a06e
Melinne Diniz Oliveira <61233511+melinnediniz@users.noreply.github.com>
2023-02-05 03:01:08 -0300

    :sparkles: finish back-end of company sign-up

--- src/api/routes/company.ts
+++ src/api/routes/company.ts
@@ -1,33 +1,24 @@
 import { Request, Router } from "express";
-import { Company } from "../../domain/entities/Company";
+import { Company } from "../../domain/entities/company";
 import { Create } from "../../domain/useCases/company/Create";
-import { CompanyService, CompanyModel } from "../services/company_service";
+import { CompanyService } from "../services/company_service";
 
 export const CompanyRouter = Router();
 
-CompanyRouter.post("/company",
+CompanyRouter.post(
+    "/company",
     async (request: Request<{}, {}, Company, {}>, response) => {
         const { body: newCompany } = request;
         try {
             const createCompany = new Create(new CompanyService());
             const createdCompany = await createCompany.execute(newCompany);
             return response.status(200).json(createdCompany);
-        } catch (error) {
-            console.log(error)
+        } catch (error: any) {
             return response.status(400).json({
                 status: 400,
-                message: error || "Company was not created.",
+                message: error?.message || "Company was not created.",
                 date: new Date(),
             });
         }
     }
 );
-
-CompanyRouter.get('/company', async (req, res) => {
-    try {
-      const companies = await CompanyModel.find();
-      res.json(companies);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  });
